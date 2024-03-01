17fc0072fd13130a87f14dc210047f4119d038d6
Bintang Rahmatullah <rahmatbintang181@gmail.com>
2024-03-01 16:09:10 +0700

    feat: added slide navigation

---
+++ app/Http/Livewire/Components/SlideNav.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use Livewire\Component;
+
+class SlideNav extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.slide-nav');
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,9 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="bg-white shadow-sm overflow-show dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <livewire:to-do.create-todo />
+
+                    <livewire:to-do.create-to-do />
+
                 </div>
             </div>
         </div>
